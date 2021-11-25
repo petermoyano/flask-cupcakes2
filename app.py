5530e7b72d967bd225aeacd4a7bf8f6184c7bd84
Pedro Moyano <pedromoyano454@gmail.com>
2021-11-25 18:27:33 -0300

    All API routes created and tests passed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import Cupcake, db, connect_db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
@@ -16,7 +16,44 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
-@app.route("/")
-def home_page():
-    """Returns home page"""
-    return "hello"
+@app.route("/api/cupcakes")
+def json_all_cupcakes():
+    """Returns json with all cupcakes"""
+    all_cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in all_cupcakes]
+    return jsonify(cupcakes=serialized)
+
+@app.route("/api/cupcakes", methods=["POST"])
+def post_one_cupcake():
+    """POST request to create a new cupcake"""
+    data = request.json
+    new_cupcake = Cupcake(flavor=data["flavor"], rating=float(data["rating"]), size=data["size"], image=data["image"] or None)
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=new_cupcake.serialize()), 201)
+
+@app.route("/api/cupcakes/<int:id>")
+def json_one_cupcake(id):
+    """Returns json with cupcake info"""
+    c = Cupcake.query.get_or_404(id)
+    serialized = c.serialize()
+    return jsonify(cupcake=serialized)
+
+
+@app.route("/api/cupcakes/<int:id>", methods=["PATCH"])
+def patch_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    cupcake.flavor = request.json.get('flavor', cupcake.flavor)  
+    cupcake.size = request.json.get('size', cupcake.size)  
+    cupcake.rating = request.json.get('rating', cupcake.rating)  
+    cupcake.image = request.json.get('image', cupcake.image)
+    db.session.commit()  
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route("/api/cupcakes/<int:id>", methods=["DELETE"])
+def delete_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    db.session.delete(cupcake)
+    db.session.commit()
+    return jsonify({"message": "Deleted"})
--- models.py
+++ models.py
@@ -7,3 +7,16 @@ def connect_db(app):
     """From within app.py we import this function to connect our Flask app to our db"""
     db.app = app
     db.init_app(app)
+
+class Cupcake(db.Model):
+    __tablename__="cupcakes"
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor=db.Column(db.Text, nullable=False)
+    size=db.Column(db.Text, nullable=False)
+    rating=db.Column(db.Float, nullable=False)
+    image=db.Column(db.Text, nullable=False, default="https://tinyurl.com/demo-cupcake")
+
+    def serialize(self):
+        """Serializes an instance of Cupcake"""
+        return {"id": self.id, "flavor": self.flavor, "size": self.size, "rating":self.rating, "image":self.image }
+
